464242e39813ee960604ae6b1be1e3225d56dc3d
a7cha <akuarung83@gmail.com>
2020-10-03 16:07:44 +0700

    tic-tac-toe react

---
+++ build/precache-manifest.6c1d0601d2d6061bd9fb36ca1454c327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae4ef248f7cd91cd8e782c8a02abaf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fad0dacdd77d9d207e1",
+    "url": "/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "9e3a17a0eb48809bb104",
+    "url": "/static/js/2.4494adcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4494adcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fad0dacdd77d9d207e1",
+    "url": "/static/js/main.2ea1b905.chunk.js"
+  },
+  {
+    "revision": "5a092276a519fa5d4df8",
+    "url": "/static/js/runtime-main.aa2ecf4d.js"
+  }
+]);
